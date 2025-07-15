bb6eee3e59eb7d101d305fd59009dffb180d536c
imkunet <im@kunet.dev>
2025-07-15 03:14:27 -0400

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      fenix,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        f = fenix.packages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ f.default.toolchain ];
+        };
+      }
+    );
+}
